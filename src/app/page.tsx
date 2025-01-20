9eb32ff10efafd69bc11959830b747b511fd6432
ziny0216 <rkfwoa02@naver.com>
2025-01-20 14:33:15 +0900

    style: prettier, eslint 설정
    prettier 설정하고 eslint와 충돌하지 않도록 패키지 설치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import styles from './page.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-
-    </div>
-  );
+  return <div className={styles.page}></div>;
 }
