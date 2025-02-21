6f9b04d915b5ff5050eda31fdfbff4d08e88a25c
ziny0216 <rkfwoa02@naver.com>
2025-02-21 15:40:23 +0900

    feat: supabase 미들웨어 생성
    supabase 세션 유지를 위한 미들웨어 생성, 로그인 되지 않은 사용자 리디렉션

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { updateSession } from '@/utils/supabaseMiddleware';
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
+    console.log('Redirecting to /home'); // ✅ 이 로그는 찍힐 가능성이 높음
     return NextResponse.redirect(new URL('/home', request.url));
   }
-  return NextResponse.next();
+  console.log('updateSession 실행 전'); // ❌ 이 로그는 찍히지 않을 가능성이 높음
+  return await updateSession(request);
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
+};
--- src/utils/supabaseServer.ts
+++ src/utils/supabaseServer.ts
@@ -4,7 +4,6 @@ import { Database } from '@/types/database.types';
 
 export async function createClient() {
   const cookieStore = await cookies();
-
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
