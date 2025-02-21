f1169c7e1265377746ea99a10f9c25dce28a408b
ziny0216 <rkfwoa02@naver.com>
2025-02-21 16:11:29 +0900

    fix: 프로필 스타일 컴포넌트 빌드오류
    주석 처리

--- src/components/module/Post/PostItem.tsx
+++ src/components/module/Post/PostItem.tsx
@@ -1,18 +1,12 @@
 import styles from '@/components/module/Post/Post.module.css';
-import StyledProfile from '@/components/module/User/Profile';
 import PostActionBar from '@/components/module/Post/PostActionBar';
 import PostImageList from '@/components/module/Post/PostImageList';
 import Link from 'next/link';
 
-const user = {
-  profile_img: 'https://picsum.photos/32/32',
-  nickname: '조여진',
-  is_anonymity: true,
-};
 export default function PostItem({ type }: { type: 'list' | 'detail' }) {
   return (
     <div className={styles.post_container}>
-      <StyledProfile user={user} />
+      {/*<StyledProfile user={user} />*/}
       <div className={styles.post_content}>
         <Link href={'/'} className={type === 'list' ? 'ellipse3' : ''}>
           (단양=연합뉴스) 윤우용 기자 = 충북도교육청은 4일 단양군과 함께 단양
