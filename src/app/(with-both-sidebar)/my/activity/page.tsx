13ade9c2aa01cc8d0f5d7781e5d87bfdc18a67a9
ziny0216 <rkfwoa02@naver.com>
2025-04-08 14:05:15 +0900

    fix: 댓글 빌드 오류 수정
    - 내 활동/댓글 mock 데이터 추가

--- src/app/(with-both-sidebar)/my/activity/page.tsx
+++ src/app/(with-both-sidebar)/my/activity/page.tsx
@@ -9,6 +9,15 @@ const tabMok: DefaultObj[] = [
   { name: '좋아요한 글', value: 'post' },
   { name: '작성한 댓글', value: 'comment' },
 ];
+
+const dataMok = {
+  comment: '1111',
+  created_at: '2024-05-06',
+  id: 123,
+  post_id: '123123',
+  user_id: '1q23123',
+  user: null,
+};
 export default function Page() {
   const [tab, setTab] = useState('post');
   const handTabChange = (value: string) => {
@@ -19,7 +28,7 @@ export default function Page() {
     <div className="inner flex-col">
       <TabList tabList={tabMok} onChange={handTabChange} />
       {/*<div>{tab === 'post' ? <PostItem type={'list'} /> : <CommentItem />}</div>*/}
-      <div>{tab === 'post' && <CommentItem />}</div>
+      <div>{tab === 'post' && <CommentItem comment={dataMok} />}</div>
     </div>
   );
 }
