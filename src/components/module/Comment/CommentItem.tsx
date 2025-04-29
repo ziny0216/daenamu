d5b2bf53aebf7957fd5ccf18f50c88dab007ef59
ziny0216 <rkfwoa02@naver.com>
2025-04-29 17:36:11 +0900

    feat: 댓글 신고 구현완료
    - 게시물, 댓글 중복코드 제거 예정

--- src/components/module/Comment/CommentItem.tsx
+++ src/components/module/Comment/CommentItem.tsx
@@ -2,15 +2,38 @@ import styles from '@/components/module/Comment/Comment.module.css';
 import { Tables } from '@/types/database.types';
 import { PostWriter } from '@/types/components/post';
 import Profile from '@/components/module/User/Profile';
+import Button from '@/components/common/Button';
+import MoreIcon from '@/assets/icons/icon-more.svg';
+import React from 'react';
 
 export default function CommentItem({
   comment,
+  handleCmtMore,
 }: {
   comment: Tables<'comments'> & PostWriter;
+  handleCmtMore?: (
+    e: React.MouseEvent<HTMLButtonElement>,
+    comment: Tables<'comments'> & PostWriter,
+  ) => void;
 }) {
+  const handleMore = (e: React.MouseEvent<HTMLButtonElement>) => {
+    handleCmtMore?.(e, comment);
+  };
+
   return (
     <div className={styles.comment_container}>
-      <Profile profile={comment.user} is_anonymity={false} />
+      <div className={styles.pf_header}>
+        <Profile profile={comment.user} is_anonymity={false} />
+        {comment.user?.nickname !== '탈퇴한 사용자' && (
+          <Button
+            className={'tooltip_trigger'}
+            onClick={handleMore}
+            icon={<MoreIcon />}
+            isIcon
+            color={'transparent'}
+          />
+        )}
+      </div>
       <div className={styles.comment_content}>
         <p className={styles.comment}>{comment.comment}</p>
       </div>
--- src/components/module/Comment/CommentList.tsx
+++ src/components/module/Comment/CommentList.tsx
@@ -4,9 +4,15 @@ import CommentItem from '@/components/module/Comment/CommentItem';
 import browserClient from '@/utils/supabaseClient';
 import { CommentWriteType } from '@/types/components/comment';
 import { toast } from 'react-toastify';
-import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 import { Tables } from '@/types/database.types';
 import { PostWriter } from '@/types/components/post';
+import { useTooltip } from '@/components/common/Tooltip';
+import { useModal } from '@/hooks/common/useModal';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/lib/store';
+import ReportModal from '@/components/modal/ReportModal';
+import { ReportReasonType } from '@/types/components/module';
 
 export default function CommentList({
   postId,
@@ -19,6 +25,11 @@ export default function CommentList({
   const [commentList, setCommentList] = useState<
     (Tables<'comments'> & PostWriter)[]
   >([]);
+  const [cmtId, setCmtId] = useState<number>();
+  const userProfile = useSelector((state: RootState) => state.user.users);
+  const { setTooltipState, tooltipState, selectedItem } = useTooltip();
+  const { openConfirmModal } = useModal();
+  const [isReportModal, setIsReportModal] = useState(false);
 
   const onSubmitComment = async (params: CommentWriteType & PostWriter) => {
     const { comment, user_id, user } = params;
@@ -42,6 +53,38 @@ export default function CommentList({
     }
   };
 
+  const handleCmtMore = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    cmt: Tables<'comments'> & PostWriter,
+  ) => {
+    e.stopPropagation();
+    const rect = e.currentTarget.getBoundingClientRect();
+    const position = {
+      x: rect.left + window.scrollX - 154,
+      y: rect.bottom + window.scrollY + 12,
+    };
+    setCmtId(cmt.id);
+
+    let defaultList = [{ name: '신고하기', value: 'report' }];
+    defaultList =
+      userProfile.id === cmt.user?.user_id
+        ? [{ name: '삭제하기', value: 'delete' }]
+        : defaultList;
+
+    if (
+      tooltipState.visible &&
+      tooltipState.position.x === position.x &&
+      tooltipState.position.y === position.y
+    ) {
+      return setTooltipState(prev => ({ ...prev, visible: false }));
+    }
+    setTooltipState({
+      visible: true,
+      list: defaultList,
+      position,
+    });
+  };
+
   useEffect(() => {
     const fetchComments = async () => {
       const { data, error } = await browserClient.rpc('get_comment_with_user', {
@@ -55,12 +98,55 @@ export default function CommentList({
     };
     fetchComments();
   }, []);
+
+  useEffect(() => {
+    if (selectedItem?.value === 'report') {
+      setIsReportModal(true);
+    }
+
+    if (selectedItem?.value === 'delete') {
+      openConfirmModal({
+        modalText: '삭제하시겠습니까?',
+        onConfirm: async () => {},
+      });
+    }
+  }, [selectedItem]);
+
+  const handleConfirm = async (report: ReportReasonType) => {
+    setIsReportModal(false);
+
+    openConfirmModal({
+      modalText: '신고하시겠습니까?',
+      onConfirm: async () => {
+        const { error } = await browserClient.from('reports').insert({
+          target_type: 'comment',
+          target_id: String(cmtId),
+          reporter_id: userProfile.id,
+          ...report,
+        });
+
+        if (error) {
+          if (error.code === '23505') {
+            return toast('이미 신고되었습니다.');
+          }
+          return toast(error.message);
+        }
+
+        toast('신고되었습니다.');
+      },
+    });
+  };
   return (
     <>
       <CommentWrite onSubmit={onSubmitComment} />
       {commentList.map(cmt => (
-        <CommentItem key={cmt.id} comment={cmt} />
+        <CommentItem handleCmtMore={handleCmtMore} key={cmt.id} comment={cmt} />
       ))}
+      <ReportModal
+        onClickCancel={() => setIsReportModal(false)}
+        isReportModal={isReportModal}
+        handleConfirm={handleConfirm}
+      />
     </>
   );
 }
